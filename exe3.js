120e919f7d18c41003db484257e90e6451e1b0b5
andriansy4h <andrikilua@gmail.com>
2018-03-02 08:47:01 +0700

    exe3.js

---
+++ exe3.js
@@ -0,0 +1,19 @@
+var nama = 'andri';
+var peran = '';
+
+if (nama === '') {
+    console.log('nama harus diisi');
+}   else if (peran ===''); {
+    console.log(halo, '+nama+' 'pilih peran kamu');
+}   else if (peran === 'ksatria') {
+    console.log('halo' +peran+ '' +nama);
+    console.log('kamu dapat menyerang dengan senjatamu');
+}   else if (peran == 'tabib');{
+    console.log('halo' +peran+ '' +nama)
+    console.log('kamu dapat membantu temanmu yang terluka');
+}   els if (peran == 'penyihir')   
+    console.log('halo' +peran+ '' +nama)
+    console.log('kamu dapat memberikan temanmu sihir')
+}   else {
+    console.log('peran yang kamu isi tidak benar')
+}
